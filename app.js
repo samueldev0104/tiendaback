f0431bdf6b3649690cb50f23033053e7ebf9653f
cubillos1395 <54371574+cubillos1395@users.noreply.github.com>
2022-12-18 23:24:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const PersonasRouter = require('./routers/PersonasRouter');
 const UsuariosRouter = require('./routers/UsuariosRouter');
+const ProductosRouter = require('./routers/ProductosRouter');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,6 +14,9 @@ app.use(bodyParser.json());
 app.use("/personas", PersonasRouter);
 //Usuarios
 app.use("/usuarios", UsuariosRouter);
+//Productos
+app.use("/productos", ProductosRouter);
 
 app.listen(5000);
+
 
