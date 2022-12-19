59d3e323973612bacdbd4955955170478355e744
cubillos1395 <54371574+cubillos1395@users.noreply.github.com>
2022-12-18 23:26:17 -0500

    Update conectionDB.js

--- db/conectionDB.js
+++ db/conectionDB.js
@@ -1,5 +1,5 @@
 const mongoose = require ('mongoose');
-const conexion = "mongodb+srv://root:admin@cluster0.kfie5kj.mongodb.net/ProyectoC4?retryWrites=true&w=majority";
+const conexion = "mongodb+srv://admin:1234@cluster0.ukfgmul.mongodb.net/tienda?retryWrites=true&w=majority";
 mongoose.connect(conexion)
     .then(evento => console.log("Conectado a Mongo"))
     .catch(error => console.log(error));
